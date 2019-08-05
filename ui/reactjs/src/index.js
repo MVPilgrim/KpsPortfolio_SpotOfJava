3ce30a00f215cae92f6b1a91eee81c6af6b6de4d
MVPilgrim <mv.pilgrim.aws@gmail.com>
2019-08-05 22:06:43 +0000

    Move UI to website.

--- ui/reactjs/src/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css'
-
-import React from 'react'
-import {render} from 'react-dom'
-
-import StandaloneApp from './StandaloneApp'
-
-render(<StandaloneApp/>, document.querySelector('#app'))
